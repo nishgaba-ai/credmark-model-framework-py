6c8d95351a95ebb0ca47b17bc8df7f0fb4700297
iwyrkore <iwyrkore@gmail.com>
2022-03-29 14:21:39 -0400

    Updated conf.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,8 @@ myst_enable_extensions = [
     "colon_fence",
 ]
 
+autosummary_generate = True
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
